f2cbe9cc0bbc0280514d9dc20589069fb72a47cb
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2024-08-08 21:13:04 +0530

    commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold text-center my-4">GSX STORE</h1>
+      <Button className="">Click me t</Button>
+    </main>
+  );
+}
